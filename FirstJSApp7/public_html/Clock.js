24c6c224ef4619f65c6707031276ec609391323d
RyanFin <21296921@student.uwl.ac.uk>
2017-02-18 20:27:18 +0000

    Added clock program

---
+++ FirstJSApp7/public_html/Clock.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Define a function to display the current time
+function displayTime() {
+    var elt = document.getElementById("clock"); //find element with the id clock
+    var now = new Date(); //get current time
+    elt.innerHTML = now.toLocaleTimeString(); // make elt display it
+    setTimeout(displayTime, 1000); //run again in 1 second
+}
+window.onload = displayTime(); //start displaying the time when document loads
